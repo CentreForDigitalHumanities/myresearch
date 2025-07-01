ec750e0c5b9ba3c62af434b92ca74d72b9a1f1c2
Edo Storm <emtstorm@hotmail.com>
2025-07-01 10:42:18 +0200

    style: black

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -6,26 +6,26 @@ from django.forms.widgets import TextInput
 from .models import User
 
 
-
-
 class UserAdmin(BaseUserAdmin):
     fieldsets = (
-        (None, {
-            'fields': ('username', 'password')
-        }),
-        ('Personal info', {
-            'fields': ('first_name', 'last_name', 'email')
-        }),
-        ('Permissions', {
-            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups',
-                       'user_permissions'),
-        }),
-        ('Important dates', {
-            'fields': ('last_login', 'date_joined')
-        }),
+        (None, {"fields": ("username", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "email")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
-    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    list_display = ("username", "email", "first_name", "last_name", "is_staff")
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
 
 
 admin.site.register(User, UserAdmin)
