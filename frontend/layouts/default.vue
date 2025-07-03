a116a88d4f59d7029747e9c499bf0205ec8cf408
Tijmen Baarda <tijmenbaarda@gmail.com>
2025-07-03 16:18:35 +0200

    Add provisional index and error pages and default layout

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const title = useAppConfig().globalTitle;
+</script>
+
+<template>
+    <div class="uu-root-container">
+        <Head>
+            <Title>{{ title }}</Title>
+        </Head>
+        <LayoutHeader />
+        <LayoutNavBar />
+        <slot />
+        <LayoutFooter />
+    </div>
+</template>
+
+<style scoped>
+</style>
