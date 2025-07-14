2fae0a5d6cd02030a27e33074e66da7d30831a8a
Edo Storm <emtstorm@hotmail.com>
2025-07-14 11:00:03 +0200

    fix: remove accidentally committed dev db settings

--- backend/myresearch/settings.py
+++ backend/myresearch/settings.py
@@ -87,12 +87,6 @@ DATABASES = {
         "PORT": "5432",
     }
 }
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": "mydatabase",
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
