9bd819a9b708c20c9e6a7dd8a32df9ee3b72c81a
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2025-07-21 14:51:11 +0200

    FormStepper component

---
+++ frontend/components/FormStepper.vue
@@ -0,0 +1,60 @@
+<script lang="ts" setup>
+import { useTranslateableAttribute } from "~/composables/useTranslation";
+
+interface FormStep {
+  labelNl: string;
+  labelEn: string;
+  children: FormStep[];
+  completed: boolean;
+  active: boolean;
+  disabled: boolean;
+}
+
+export interface FormStepperConfig {
+  title: string;
+  steps: Array<FormStep>;
+}
+
+interface Props {
+  stepConfig: FormStepperConfig;
+}
+
+const props = defineProps<Props>();
+
+function stepperItemClasses(step: FormStep): string {
+  const classes: string[] = ["stepper-item"];
+  if (step.active) {
+    classes.push("active");
+  }
+  if (step.completed) {
+    classes.push("complete");
+  }
+  if (step.disabled) {
+    classes.push("disabled");
+  }
+  return classes.join(" ");
+}
+</script>
+
+<template>
+  <div class="stepper">
+    <ul>
+      <li v-for="(step, index) in props.stepConfig.steps" :key="index">
+        <a :class="stepperItemClasses(step)">
+          <span class="stepper-bubble stepper-bubble-largest">{{
+            index + 1
+          }}</span>
+          <span>{{ useTranslateableAttribute(step, "label") }}</span>
+        </a>
+        <ul v-if="step.children.length > 0">
+          <li v-for="(child, childIndex) in step.children" :key="childIndex">
+            <a :class="stepperItemClasses(child)">
+              <span class="stepper-bubble stepper-bubble-large"></span>
+              <span>{{ useTranslateableAttribute(child, "label") }}</span>
+            </a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
--- frontend/plugins/i18n.ts
+++ frontend/plugins/i18n.ts
@@ -1,27 +1,29 @@
-import {createI18n} from "vue-i18n";
+import { createI18n } from "vue-i18n";
 import nl from "../locales/nl.json";
 
 const messages = {
-    nl
+  nl,
 };
 const defaultLocale = "en";
 
 /* Get locale from local storage or, if not available, from the browser
    data, ignoring the country part (i.e. nl-NL becomes nl).
  */
-let locale: string = localStorage.locale ?? navigator.language.split("-")[0];
-if (!(locale in messages)) {
-    locale = "en";
-}
+const locale =
+  localStorage.getItem("locale") ||
+  navigator.language.split("-")[0] ||
+  defaultLocale;
 
 export const i18n = createI18n({
-    globalInjection: true,
-    locale,
-    messages,
-    formatFallbackMessages: true,
-    fallbackLocale: defaultLocale,
+  // Use the newer Composition API
+  legacy: false,
+  globalInjection: true,
+  locale,
+  messages,
+  formatFallbackMessages: true,
+  fallbackLocale: defaultLocale,
 });
 
-export default defineNuxtPlugin(({ vueApp}) => {
-    vueApp.use(i18n);
+export default defineNuxtPlugin(({ vueApp }) => {
+  vueApp.use(i18n);
 });
