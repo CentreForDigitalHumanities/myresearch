25f0cd00bc673c6a13a0a8c231b111946fa4c2a1
Edo Storm <emtstorm@hotmail.com>
2025-07-29 14:55:22 +0200

    fix: remove temporary docstring from UserType

--- backend/main/types/UserType.py
+++ backend/main/types/UserType.py
@@ -8,10 +8,6 @@ from main.models import User
 
 
 class UserType(DjangoObjectType):
-    """
-    NOTE: This Type was written during a very early phase of development as 
-    a proof-of-concept for implementing graphql
-    """
     class Meta:
         model = get_user_model()
         fields = ["id", "username", "email"]
