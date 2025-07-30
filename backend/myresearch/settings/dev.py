0ac4e5e049e403809a86c268fce46f5f0e286fd0
Michael Villeneuve <m.l.villeneuve@uu.nl>
2025-07-30 14:38:11 +0200

    feat: Separate Django settings files between dev and prod

---
+++ backend/myresearch/settings/dev.py
@@ -0,0 +1,12 @@
+from .utils import discover
+
+DEBUG = True
+SECRET_KEY = "django-insecure-s8e=1!*6dzct5!vn$0%qdc!x4$_vhd895g0a1#e$_v+oqbvvyq"
+
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "mr-django",
+]
+
+from .generic_settings import *
