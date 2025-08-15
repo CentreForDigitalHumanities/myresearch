025845418cc3938ae11182c6fd4deff22aa5f586
Frank Roordink <frank.roordink@live.nl>
2025-08-15 14:10:57 +0200

    added navigational tiles

---
+++ frontend/components/RecentActivity.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="tiles uu-container">
+    <a class="tile">
+      <NuxtLink to="/studies/" class="nav-link">
+        <div class="row justify-content-between">
+          <div class="col-8">
+            <strong>refnumber + Titelmet langere naam</strong>
+          </div>
+          <div class="col-4">25-08-23</div>
+        </div>
+        <p></p>
+        <div class="row justify-content-between">
+          <div class="col-6">TYPE</div>
+          <div class="col-6">Wegwijzer</div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-6">Status</div>
+          <div class="col-6">DRAFT</div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-6">fav color</div>
+          <div class="col-6">geel</div>
+        </div>
+        <div class="row justify-content-start">
+          <div class="col-6">fav color</div>
+          <div class="col-6">Geel</div>
+        </div>
+        <div class="row justify-content-end">
+          <button class="btn btn-arrow-right col-6">Continue</button>
+        </div>
+      </NuxtLink>
+    </a>
+    <a class="tile">
+      <NuxtLink to="/studies/" class="nav-link">
+        <div class="row">
+          <div class="col-8">
+            <strong>refnumber + Titelmet langere naam</strong>
+          </div>
+          <div class="col-4">25-08-23</div>
+        </div>
+        <p></p>
+        <div class="row">
+          <div class="col-6">TYPE</div>
+          <div class="col-6">Wegwijzer</div>
+        </div>
+        <div class="row">
+          <div class="col-6">Status</div>
+          <div class="col-6">DRAFT</div>
+        </div>
+        <div class="row">
+          <div class="col-6">fav color</div>
+          <div class="col-6">
+            geel met een hele lange tekst erachter geplakt
+          </div>
+        </div>
+        <div class="row justify-content-end">
+          <button class="btn btn-arrow-right col-6">Continue</button>
+        </div>
+      </NuxtLink>
+    </a>
+  </div>
+</template>
+
+<style scoped></style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -5,37 +5,42 @@
     <div class="uu-hero">
       <h1>{{ $t("Welcome to MyResearch!") }}</h1>
     </div>
-
-    <div class="uu-container">
-      {{
-        $t(
-          "My research is een tool dat onderzoekers helpt met het aanvragen van een onderzoek. " +
-            "Op deze website kan uw onderzoek worden geregistreerd bij het verwerkingsregister en " +
-            "een aanvraag kan worden gedaan bij de ethische commissie of uw onderzoek wel door de beugal kan. " +
-            "Maar eerst is het de bedoeling dat een wegwijzer wordt ingevuld. Het kan namelijk zo zijn " +
-            "dat uw onderzoek bepaalde aanvragen helemaal niet hoeft te doen. " +
-            "Iets wat de wegwijzer samen met u uitzoekt.",
-        )
-      }}
+    <div class="uu-sidebar-container uu-sidebar-right">
+      <aside class="uu-sidebar">
+        <RecentActivity />
+      </aside>
+      <div class="uu-sidebar-content">
+        <div class="uu-container">
+          {{
+            $t(
+              "My research is een tool dat onderzoekers helpt met het aanvragen van een onderzoek. " +
+                "Op deze website kan uw onderzoek worden geregistreerd bij het verwerkingsregister en " +
+                "een aanvraag kan worden gedaan bij de ethische commissie of uw onderzoek wel door de beugal kan. " +
+                "Maar eerst is het de bedoeling dat een wegwijzer wordt ingevuld. Het kan namelijk zo zijn " +
+                "dat uw onderzoek bepaalde aanvragen helemaal niet hoeft te doen. " +
+                "Iets wat de wegwijzer samen met u uitzoekt.",
+            )
+          }}
+        </div>
+        <div class="uu-container">
+          <h2>{{ $t("Verwerkingsregister") }}</h2>
+          <p>
+            {{
+              $t(
+                "Bij het verwerkingsregister start je de registratie van studies (onderzoeksprojecten) " +
+                  "waarin er persoonsgegevens worden verwerkt. Het verwerkingsregister is een centraal " +
+                  "overzicht van alle studies waarin persoonsgegevens een rol spelen. De Algemene Verordening " +
+                  "Gegevensbescherming (AVG) eist dat dergelijke studies worden geregistreerd. Voor de onderzoeker z" +
+                  "elf biedt het verwerkingsregister ook voordelen: het geeft op een gebruiksvriendelijke manier inzicht " +
+                  "in allerlei aspecten van de gegevensverwerking, zoals de typen persoonsgegevens, de personen op wie die " +
+                  "gegevens van toepassing zijn, de bewaartermijnen, de gebruikte software, de beveiligingsmaatregelen en " +
+                  "de verschillende partijen die een rol spelen.",
+              )
+            }}
+          </p>
+        </div>
+      </div>
     </div>
-    <div class="uu-container">
-      <h2>{{ $t("Verwerkingsregister") }}</h2>
-      <p>
-        {{
-          $t(
-            "Bij het verwerkingsregister start je de registratie van studies (onderzoeksprojecten) " +
-              "waarin er persoonsgegevens worden verwerkt. Het verwerkingsregister is een centraal " +
-              "overzicht van alle studies waarin persoonsgegevens een rol spelen. De Algemene Verordening " +
-              "Gegevensbescherming (AVG) eist dat dergelijke studies worden geregistreerd. Voor de onderzoeker z" +
-              "elf biedt het verwerkingsregister ook voordelen: het geeft op een gebruiksvriendelijke manier inzicht " +
-              "in allerlei aspecten van de gegevensverwerking, zoals de typen persoonsgegevens, de personen op wie die " +
-              "gegevens van toepassing zijn, de bewaartermijnen, de gebruikte software, de beveiligingsmaatregelen en " +
-              "de verschillende partijen die een rol spelen.",
-          )
-        }}
-      </p>
-    </div>
-    <div class="uu-container"></div>
     <div class="uu-sidebar-container uu-sidebar-right">
       <div class="uu-sidebar-content">
         <h2 class="uu-sidebar-header-linked">Veelgestelde vragen</h2>
@@ -45,6 +50,7 @@
         <h2 class="uu-sidebar-header-linked">Contact</h2>
         <!-- header stripe does not yet work here -->
         <p>
+          <!-- mail should probably come from a db or config file, that way rebo implementation will be easier -->
           {{ $t("Voor vragen neem contact op met de privacy officier: ")
           }}<a href="mailto:l.j.w.m.kessels@uu.nl">l.j.w.m.kessels@uu.nl</a>
         </p>
