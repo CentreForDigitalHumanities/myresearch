d1739920c9eed40478cd3a4135ec8948efb19325
Frank Roordink <frank.roordink@live.nl>
2025-09-22 14:16:30 +0200

    Links can be now used in text. prettier fixes url translation does not yet work.

--- frontend/components/frontpage/FrequentQuestions.vue
+++ frontend/components/frontpage/FrequentQuestions.vue
@@ -1,51 +1,77 @@
 <script lang="ts" setup>
-import { MockData, type MockQuestionKey } from "~/shared/mockData";
+import {
+    type AnswerPart,
+    type LinkData,
+    type MockQuestionKey,
+    mockQuestions,
+} from "~/shared/mockData";
 
 interface Props {
-  questionsGroup: MockQuestionKey;
-  title: string;
+    questionsGroup: MockQuestionKey;
+    title: string;
 }
 const props = defineProps<Props>();
 
-const questionAnswers: Record<string, string> =
-  MockData.mockQuestions[props.questionsGroup];
+const questionAnswers: Record<string, AnswerPart[]> =
+    mockQuestions[props.questionsGroup];
+
+const isLinkData = (obj: LinkData | string): obj is LinkData => {
+    return obj.hasOwnProperty("url");
+};
 </script>
 
 <template>
-  <div class="mw-100">
-    <h2 class="uu-sidebar-header-linked">
-      {{ $t(title) }}
-    </h2>
-    <div class="accordion mw-100" :id="'accordion' + questionsGroup">
-      <div
-        class="mw-100"
-        v-for="(answer, question, index) in questionAnswers"
-        :key="questionsGroup + index"
-      >
-        <div class="accordion-item mw-100">
-          <h2 class="accordion-header">
-            <button
-              class="accordion-button"
-              type="button"
-              data-bs-toggle="collapse"
-              :data-bs-target="'#collapse' + questionsGroup + index"
-              aria-expanded="false"
-              :aria-controls="'collapse' + questionsGroup + index"
+    <div class="mw-100">
+        <h2 class="uu-sidebar-header-linked">
+            {{ $t(title) }}
+        </h2>
+        <div :id="'accordion' + questionsGroup" class="accordion mw-100">
+            <div
+                v-for="(answerParts, question, index) in questionAnswers"
+                :key="questionsGroup + index"
+                class="mw-100"
             >
-              {{ $t(question) }}
-            </button>
-          </h2>
-          <div
-            :id="'collapse' + questionsGroup + index"
-            class="accordion-collapse collapse mw-100"
-            :data-bs-parent="'#accordion' + questionsGroup"
-          >
-            <div class="accordion-body mw-100">
-              {{ $t(answer) }}
+                <div class="accordion-item mw-100">
+                    <h2 class="accordion-header">
+                        <button
+                            class="accordion-button"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            :data-bs-target="
+                                '#collapse' + questionsGroup + index
+                            "
+                            aria-expanded="false"
+                            :aria-controls="'collapse' + questionsGroup + index"
+                        >
+                            {{ $t(question) }}
+                        </button>
+                    </h2>
+                    <div
+                        :id="'collapse' + questionsGroup + index"
+                        class="accordion-collapse collapse mw-100"
+                        :data-bs-parent="'#accordion' + questionsGroup"
+                    >
+                        <div class="accordion-body mw-100">
+                            <template
+                                v-for="(answerPart, answerIndex) in answerParts"
+                                :key="answerIndex"
+                            >
+                                <template v-if="isLinkData(answerPart)">
+                                    <NuxtLink :to="answerPart.url">
+                                        <!-- still untranslated, something with slot can help? -->
+                                        {{ $t(answerPart.text) }}
+                                    </NuxtLink>
+                                </template>
+                                <template v-else>
+                                    {{ $t(answerPart) }}
+                                </template>
+                                {{ " " }}
+                                <!-- I just want a space here there has to be a better way to do this. -->
+                            </template>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
--- frontend/shared/mockData.ts
+++ frontend/shared/mockData.ts
@@ -1,32 +1,42 @@
 export type MockQuestionKey =
-  | "processingRegistry"
-  | "ethicalCommission"
-  | "other";
+    | "processingRegistry"
+    | "ethicalCommission"
+    | "other";
 
-type MockQuestions = Record<MockQuestionKey, Record<string, string>>;
-
-export class MockData {
-  static mockQuestions: MockQuestions = {
+export type LinkData = {
+    text: string;
+    url: string;
+};
+export type AnswerPart = string | LinkData;
+type MockQuestions = Record<MockQuestionKey, Record<string, AnswerPart[]>>;
+export const mockQuestions: MockQuestions = {
     processingRegistry: {
-      "What is part of personal data?":
-        "Personal data is dat that tells something about a living person of whom you know the identity, from whom you can discern the identity or whom are otherwise recognizable. For example: contact information is personal data, but also a lot of research data is personal data. Measurement data, answers on a questionnaire, or interviews, observations, demographic data and much more. Data that can not be traced back to personal data are anonymous data, those are not personal data",
-      "Why are bananas curved?": "Because they are yellow.",
+        "What is part of personal data?": [
+            "Personal data is dat that tells something about a living person of whom you know the identity, from whom you can discern the identity or whom are otherwise recognizable. For example: contact information is personal data, but also a lot of research data is personal data. Measurement data, answers on a questionnaire, or interviews, observations, demographic data and much more. Data that can not be traced back to personal data are anonymous data, those are not personal data",
+        ],
+        "Why are bananas curved?": ["Because they are yellow."],
     },
     ethicalCommission: {
-      "Where can i find the documents I have to deliver?":
-        "During the application it may be possible that you need submit certain documents. Use the most recent model documents." +
-        " " +
-        "https://intranet.uu.nl/en/documents-ethics-assessment-committee-humanities",
-      "How long do responses take after submission?": "No idea.",
-      "What happens if my application gets rejected?":
-        "You will need to make a revision and resubmit the application.",
-      "Help my draft application suddenly changed":
-        "It is possible for the supervisor to change your application.",
+        "Where can i find the documents I have to deliver?": [
+            "During the application it may be possible that you need submit certain documents. Use the most recent model documents.",
+            {
+                text: "Model Documents",
+                url: "https://intranet.uu.nl/en/documents-ethics-assessment-committee-humanities",
+            },
+            "You can also find the links to the documents during the application itself.",
+        ],
+        "How long do responses take after submission?": ["No idea."],
+        "What happens if my application gets rejected?": [
+            "You will need to make a revision and resubmit the application.",
+        ],
+        "Help my draft application suddenly changed": [
+            "It is possible for the supervisor to change your application.",
+        ],
     },
     other: {
-      "When will the Ethical Commission be added to to My Research?":
-        "After an estimated 2000 cups of coffee.",
-      "Why am I looking at a picture of a cat?": "https://cataas.com/cat",
+        "When will the Ethical Commission be added to to My Research?": [
+            "After an estimated 2000 cups of coffee.",
+        ],
+        "Why am I looking at a picture of a cat?": ["https://cataas.com/cat"],
     },
-  };
-}
+};
