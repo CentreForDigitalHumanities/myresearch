c306b8a16271f092b91050894db1a92188d227c0
Edo Storm <emtstorm@hotmail.com>
2025-10-01 16:13:16 +0200

    feat: add group fixtures via migration

--- backend/main/models.py
+++ backend/main/models.py
@@ -3,4 +3,19 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    
+    @property
+    def is_privacy_officer(self,):
+        return "Privacy officer" in [g.name for g in self.groups.all()]
+
+    @property    
+    def is_fetc_member(self,):
+        return "FETC member" in [g.name for g in self.groups.all()]
+    
+class MRPermissionTypes(models.IntegerChoices):
+    """
+    PermissionTypes are used in ObjectType's get_queryset method, to decide 
+    which queryset gets returned, based on the user's permissions.
+    """
+    VIEW = 1
+    EDIT = 2
