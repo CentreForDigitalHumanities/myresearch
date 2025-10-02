0228b8a62abc6340629f2366227365c21c71ee59
Edo Storm <emtstorm@hotmail.com>
2025-10-02 11:41:38 +0200

    feat: add mutations

--- backend/api/graphql/schema.py
+++ backend/api/graphql/schema.py
@@ -15,13 +15,21 @@ from form.queries import FormQueries
 from main.queries import ListUsersQuery
 from study.queries import StudyQuery
 
+from study.mutations.StudyMutations import CreateStudy, UpdateStudy, DeleteStudy
+
 
 class Query(ListUsersQuery, FormQueries, StudyQuery, ObjectType):
     pass
 
+class Mutation(ObjectType):
+
+    create_study = CreateStudy.Field()
+    update_study = UpdateStudy.Field()
+    delete_study = DeleteStudy.Field()
 
 schema = Schema(
     query=Query,
+    mutation=Mutation,
     types=[
         # These types are not queried directly, so they are included here to
         # make Graphene aware of them.
---
+++ backend/study/mutations/StudyMutations.py
@@ -0,0 +1,108 @@
+from graphene import String, Mutation, Field, ID, Boolean, ResolveInfo, List
+from graphene_django.types import ErrorType
+
+from study.types.StudyType import StudyType
+from study.models import Study
+from main.models import MRPermissionTypes
+
+class CreateStudy(Mutation):
+
+    study = Field(StudyType)
+    errors = List(ErrorType)
+
+    class Arguments:
+        title = String(required=True)
+
+    @classmethod
+    def mutate(cls, root: None, info: ResolveInfo, title: str,):
+
+        user = info.context.user
+        # Initialize the study, but do not save()
+        study = Study(title=title, created_by=user)
+        print(study)
+
+        # Check if the user has Edit permission
+        if not study.can_be_accessed_by(user, MRPermissionTypes.EDIT):
+            error = ErrorType(
+                field="",
+                messages=["You are not authorized to create this study."]
+            )
+            return cls(errors=[error])
+        
+        # If permission check gets passed, save() and return the study
+        study.save()
+        return cls(study=study)
+
+
+class UpdateStudy(Mutation):
+
+    study = Field(StudyType)
+    errors = List(ErrorType)
+
+    class Arguments:
+        id = ID(required=True)
+        title = String(required=True)
+
+    @classmethod
+    def mutate(cls, root: None, info: ResolveInfo, title: str, id: int,):
+
+        study = Study.objects.get(pk=id)
+
+        # Check if the study exists.
+        if study is None:
+            error = ErrorType(
+                field="id",
+                messages=["Study not found."],
+            )
+            return cls(errors=[error])
+        
+        user = info.context.user
+
+        # Check if the user has Edit permission
+        if not study.can_be_accessed_by(user, MRPermissionTypes.EDIT):
+            error = ErrorType(
+                field="",
+                messages=["You are not authorized to update this study."]
+            )
+            return cls(errors=[error])
+
+        # If the permission check has passed, update the title.
+        study.title = title
+
+        study.save()
+        return cls(study=study)
+
+
+class DeleteStudy(Mutation):
+
+    ok = Boolean()
+    errors = List(ErrorType)
+
+    class Arguments:
+        id = ID(required=True)
+
+    @classmethod
+    def mutate(cls, root: None, info: ResolveInfo, id: int,):
+
+        user = info.context.user
+
+        study = Study.objects.get(pk=id)
+
+        # Check if the study exists.
+        if study is None:
+            error = ErrorType(
+                field="id",
+                messages=["Study not found."],
+            )
+            return cls(errors=[error])
+        
+        # Check if the user has Edit permission
+        if not study.can_be_accessed_by(user, MRPermissionTypes.EDIT):
+            error = ErrorType(
+                field="",
+                messages=["You are not authorized to delete this study."]
+            )
+            return cls(errors=[error])
+
+        study.delete()
+        return cls(ok=True)
