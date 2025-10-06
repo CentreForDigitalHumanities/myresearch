6e2aadcc4c86960df982ef0fd894202583608e84
Edo Storm <emtstorm@hotmail.com>
2025-10-06 13:40:10 +0200

    feat: make saml urls always load

--- backend/myresearch/urls.py
+++ backend/myresearch/urls.py
@@ -20,6 +20,9 @@ from django.http import HttpResponse
 from django.urls import path, include
 from django.conf import settings
 
+from djangosaml2.views import LoginView
+from cdh.federated_auth.saml.views import LogoutInitView
+
 urlpatterns = [
     path(
         "backend/",
@@ -31,20 +34,11 @@ urlpatterns = [
                 path("healthcheck/", lambda r: HttpResponse()),
             ],
         ),
-    )
+    ),
+    # SAML urls
+    path("saml/login/", LoginView.as_view(), name="login"),
+    # We can only have one logout view. Luckily, the SAML logout view can
+    # handle local accounts as well.
+    path("saml/logout/", LogoutInitView.as_view(), name="logout"),
+    path("saml/", include("djangosaml2.urls")),
 ]
-
-# If SAML is enabled, add the required URL patterns for SAML
-if hasattr(settings, "SAML_CONFIG"):
-    from djangosaml2.views import LoginView
-    from cdh.federated_auth.saml.views import LogoutInitView
-
-    urlpatterns.extend(
-        [
-            path("saml/login/", LoginView.as_view(), name="saml-login"),
-            # We can only have one logout view. Luckily, the SAML logout view can
-            # handle local accounts as well.
-            path("saml/logout/", LogoutInitView.as_view(), name="logout"),
-            path("saml/", include("djangosaml2.urls")),
-        ]
-    )
