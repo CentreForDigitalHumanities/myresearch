3175f61c5e520ac261b2cd5e80082ea7dc85f5d1
Edo Storm <emtstorm@hotmail.com>
2025-10-15 11:34:31 +0200

    style: black

--- backend/main/models.py
+++ backend/main/models.py
@@ -18,8 +18,8 @@ class User(AbstractUser):
 
 class MRPermissionTypes(models.IntegerChoices):
     """
-    PermissionTypes are used in ObjectType's get_queryset method and Model's 
-    can_be_accessed_by() method, to decide which queryset gets returned or 
+    PermissionTypes are used in ObjectType's get_queryset method and Model's
+    can_be_accessed_by() method, to decide which queryset gets returned or
     whether a user has access to an object, based on the user's permissions.
     """
 
