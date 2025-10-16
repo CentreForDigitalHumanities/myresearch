a5404642ccfe6c1af72ed234eb189f0c43e6042f
Edo Storm <emtstorm@hotmail.com>
2025-10-16 16:44:19 +0200

    fix: set max_length for Study.title

--- backend/study/migrations/0001_initial.py
+++ backend/study/migrations/0001_initial.py
@@ -26,7 +26,7 @@ class Migration(migrations.Migration):
                         verbose_name="ID",
                     ),
                 ),
-                ("title", models.CharField()),
+                ("title", models.CharField(max_length=200,)),
                 (
                     "created_by",
                     models.ForeignKey(
