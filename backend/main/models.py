d1917d71b8196ce087dee161e84bc723d345a3aa
Edo Storm <emtstorm@hotmail.com>
2025-10-16 16:12:36 +0200

    feat: make group name fixtures in User model

--- backend/main/models.py
+++ backend/main/models.py
@@ -3,17 +3,22 @@ from django.db import models
 
 
 class User(AbstractUser):
+    
+    # Some constants used for group names
+    PRIVACY_OFFICER = "Privacy officer"
+    FETC_MEMBER = "FETC member"
+
     @property
     def is_privacy_officer(
         self,
     ):
-        return "Privacy officer" in [g.name for g in self.groups.all()]
+        return self.PRIVACY_OFFICER in [g.name for g in self.groups.all()]
 
     @property
     def is_fetc_member(
         self,
     ):
-        return "FETC member" in [g.name for g in self.groups.all()]
+        return self.FETC_MEMBER in [g.name for g in self.groups.all()]
 
 
 class MRPermissionTypes(models.IntegerChoices):
