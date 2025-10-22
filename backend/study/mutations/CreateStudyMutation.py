16bac4c63e379b14ce3095763381bb8c9d1e6e81
Edo Storm <emtstorm@hotmail.com>
2025-10-22 12:00:29 +0200

    fix: CreateStudyMutation

--- backend/study/mutations/CreateStudyMutation.py
+++ backend/study/mutations/CreateStudyMutation.py
@@ -23,16 +23,16 @@ class CreateStudyMutation(Mutation):
 
         user = info.context.user
 
-        # Initialize the study, but do not save()
-        study = Study(title=title, created_by=user)
-
-        # Check if the user has Edit permission
-        if study.can_be_created_by(user):
+        # Check if the user has Create permission
+        if Study.can_be_created_by(user):
+            # Initialize the study, but do not save()
+            study = Study(title=title, created_by=user)
+            study.save()
+            return cls(study=study)
+        else:
             error = ErrorType(
                 field="", messages=["You are not authorized to create this study."]
             )
             return cls(errors=[error])
-        # If permission check gets passed, save() and return the study
-        study.save()
 
-        return cls(study=study)
+
