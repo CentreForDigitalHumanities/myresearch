8255c09f580b886b96b5557df7aa842843574cfa
Edo Storm <emtstorm@hotmail.com>
2025-10-22 09:55:31 +0200

    feat: redo create permission

--- backend/study/models.py
+++ backend/study/models.py
@@ -11,35 +11,6 @@ class Study(models.Model):
         max_length=200,
     )
 
-    def can_be_accessed_by(self, user, permission):
-        """
-        Check whether an object can be accessed by a specific user, with a
-        specific permission type.
-
-        TODO: This function should probably live in some kind off mixin for all
-        models in MyResearch.
-        """
-        if not user.is_authenticated:
-            return False
-        if permission == MRPermission.EDIT:
-            return self.can_be_edited_by(user)
-        if permission == MRPermission.VIEW:
-            return self.can_be_viewed_by(user)
-        raise ValueError(
-            f"{self}.can_be_accessed_by() has received a permission type which "
-            "has not yet been implemented."
-        )
-
-    def can_be_viewed_by(self, user):
-
-        if user == self.created_by:
-            return True
-        if user.is_privacy_officer or user.is_fetc_member:
-            return True
-        return False
-
-    def can_be_edited_by(self, user):
-
-        if user == self.created_by:
-            return True
-        return False
+    @staticmethod
+    def can_be_created_by(user):
+        return user.is_authenticated
--- backend/study/mutations/CreateStudyMutation.py
+++ backend/study/mutations/CreateStudyMutation.py
@@ -1,7 +1,5 @@
-from backend.main.models import MRPermission
-from backend.study.models import Study
-from backend.study.types.StudyType import StudyType
-
+from study.models import Study
+from study.types.StudyType import StudyType
 
 from graphene import Field, List, Mutation, ResolveInfo, String
 from graphene_django.types import ErrorType
@@ -24,16 +22,17 @@ class CreateStudyMutation(Mutation):
     ):
 
         user = info.context.user
+
         # Initialize the study, but do not save()
         study = Study(title=title, created_by=user)
 
         # Check if the user has Edit permission
-        if not study.can_be_accessed_by(user, MRPermission.EDIT):
+        if study.can_be_created_by(user):
             error = ErrorType(
                 field="", messages=["You are not authorized to create this study."]
             )
             return cls(errors=[error])
-
         # If permission check gets passed, save() and return the study
         study.save()
+
         return cls(study=study)
