625421cd97ad6e0ae5eee7740173d58cc59ab286
Edo Storm <emtstorm@hotmail.com>
2025-10-23 10:36:13 +0200

    fix: remove mrpermission from get_queryset

--- backend/study/types/StudyType.py
+++ backend/study/types/StudyType.py
@@ -4,7 +4,6 @@ from graphene_django import DjangoObjectType
 from django.db.models import QuerySet
 
 from study.models import Study
-from main.models import User, MRPermission
 
 
 class StudyType(DjangoObjectType):
@@ -21,13 +20,5 @@ class StudyType(DjangoObjectType):
         cls,
         queryset: QuerySet[Study],
         info: ResolveInfo,
-        mrpermission: str,
     ) -> QuerySet[Study]:
-        """
-        Return the queryset of studies, based on the permission, defined in the
-        query.
-        """
-
-        user = info.context.user
-
-        return queryset.accessible_objects(user, mrpermission)
+        return queryset
