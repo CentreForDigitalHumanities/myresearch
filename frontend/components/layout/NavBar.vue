35adce487a1e88b9b1ff7ec84b7985bd9f561d80
Edo Storm <emtstorm@hotmail.com>
2025-11-20 15:43:19 +0100

    feat: Add studies page to navbar

--- frontend/components/layout/NavBar.vue
+++ frontend/components/layout/NavBar.vue
@@ -56,8 +56,8 @@ const slug = computed<string | null>(() => {
                         </NuxtLink>
                     </li>
                     <li>
-                        <NuxtLink to="/" class="nav-link" active-class="active">
-                            {{ $t("My studies") }}
+                        <NuxtLink to="/studies/" class="nav-link" active-class="active">
+                            {{ $t("Studies") }}
                         </NuxtLink>
                     </li>
                     <li>
