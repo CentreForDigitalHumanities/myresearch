5f91f85e142236ae03df04aff04f76293399f98e
Frank Roordink <frank.roordink@live.nl>
2025-11-28 13:50:55 +0100

    only import collapse.

--- frontend/plugins/bootstrap.ts
+++ frontend/plugins/bootstrap.ts
@@ -1,32 +1,10 @@
-import {
-    Alert,
-    Button,
-    Collapse,
-    Dropdown,
-    Modal,
-    Offcanvas,
-    Popover,
-    ScrollSpy,
-    Tab,
-    Toast,
-    Tooltip,
-} from "bootstrap";
+import { Collapse } from "bootstrap";
 
 export default defineNuxtPlugin(() => {
     return {
         provide: {
             bootstrap: {
-                Alert,
-                Button,
                 Collapse,
-                Dropdown,
-                Modal,
-                Offcanvas,
-                Popover,
-                ScrollSpy,
-                Tab,
-                Toast,
-                Tooltip,
             },
         },
     };
