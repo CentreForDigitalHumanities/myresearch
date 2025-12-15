15f473dabae8eefa220fe943e434d185cc7aa016
Edo Storm <emtstorm@hotmail.com>
2025-12-15 11:44:59 +0100

    fix: StudyType as prop

--- frontend/components/study_detail/StudyDetailsSidebar.vue
+++ frontend/components/study_detail/StudyDetailsSidebar.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
+import type { StudyType } from '~/generated/gql/graphql';
+
 defineProps<{
-    study: any;
+    study: StudyType;
     randomDatePastYear: string;
 }>();
 </script>
