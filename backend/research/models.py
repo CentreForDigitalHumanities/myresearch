09056e7865aa0c416685d77cd1b1d99e97942ba3
Edo Storm <emtstorm@hotmail.com>
2025-12-17 11:00:34 +0100

    fix: first mockup for Study and status change

--- backend/research/models.py
+++ backend/research/models.py
@@ -3,6 +3,9 @@ from main.utils.permission_utils import BaseMRManager
 
 from django.db import models
 
+################
+# Study Object #
+################
 
 class StudyManager(BaseMRManager):
     def _viewable_objects(self, user: User):
@@ -27,3 +30,36 @@ class Study(models.Model):
         return user.is_authenticated
 
     objects = StudyManager()
+
+#######################
+# Staus Change Object #
+#######################
+
+class Statuses(models.TextChoices):
+    CREATED = "CRE"
+    SUBMITTED = "SUB"
+    APPROVED = "APP"
+    REJECTED = "REJ"
+    REVISED = "REV"
+    COMPLETED = "COM"
+
+class StatusChange(models.Model):
+
+    status = models.CharField(
+        choices=Statuses.choices
+    )
+
+    # Not currently in this branch
+
+    # user_form_submission = models.ForeignKey(
+    #     "form.UserFormSubmission",
+    #     on_delete=models.CASCADE,
+    #     related_name="status_changes",
+    # )
+
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{Statuses(self.new_status).label}: {self.changed_time.strftime("%d-%m-%Y, %H:%M")}" 
