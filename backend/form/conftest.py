686fc14f5c3c17276495acd3a59009a30f22897c
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2025-12-19 11:23:28 +0100

    Remove test_ from fixtures

--- backend/form/conftest.py
+++ backend/form/conftest.py
@@ -3,29 +3,29 @@ from form.models import MRForm, Step, TextQuestion
 
 
 @pytest.fixture
-def test_form() -> MRForm:
+def form() -> MRForm:
     return MRForm.objects.create(name="Test Form")
 
 
 @pytest.fixture
-def test_step(test_form: MRForm) -> Step:
+def step(form: MRForm) -> Step:
     """Create a test step associated with the test form."""
-    return Step.objects.create(name="Test Step", slug="test-step", form=test_form)
+    return Step.objects.create(name="Test Step", slug="test-step", form=form)
 
 
 @pytest.fixture
-def trigger_question(test_step: Step) -> TextQuestion:
+def trigger_question(step: Step) -> TextQuestion:
     """Create a trigger question for conditions."""
     return TextQuestion.objects.create(
         text="Trigger Question",
-        step=test_step,
+        step=step,
     )
 
 
 @pytest.fixture
-def target_question(test_step: Step) -> TextQuestion:
+def target_question(step: Step) -> TextQuestion:
     """Create a target question for conditions."""
     return TextQuestion.objects.create(
         text="Target Question",
-        step=test_step,
+        step=step,
     )
