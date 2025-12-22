0e7166c182ca8f15288be7084d7e433e413f7861
Edo Storm <emtstorm@hotmail.com>
2025-12-22 11:39:39 +0100

    fix: permission on StudyFormSubmission

--- backend/research/models.py
+++ backend/research/models.py
@@ -36,20 +36,33 @@ class Study(models.Model):
 # Study Form Submissions #
 ##########################
 
+class StudyFormSubmissionManager(BaseMRManager):
+
+    def _viewable_objects(self, user: User):
+        if user.is_privacy_officer or user.is_fetc_member:
+            return self.exclude(status=Statuses.DRAFT)
+        return self.filter(created_by=user)
+
+    def _editable_objects(self, user: User):
+        return self.filter(created_by=user)
+
 class StudyFormSubmission(UserFormSubmission):
     """
     Subclass of UserFormSubmission, used specifically for submitted forms that
     relate to a specific study.
     """
-
     study = models.ForeignKey(Study, on_delete=models.CASCADE, related_name="form_submissions")
+
+    @property
+    def status(self,):
+        return self.status_changes.last().status
 
 ########################
 # Status Change Object #
 ########################
 
 class Statuses(models.TextChoices):
-    CREATED = "CRE"
+    DRAFT = "DRA"
     SUBMITTED = "SUB"
     APPROVED = "APP"
     REJECTED = "REJ"
@@ -58,7 +71,8 @@ class Statuses(models.TextChoices):
 
 class StatusChange(models.Model):
     """
-    Object which handles the status of a StudyFormSubmission.
+    Object which handles the status of a StudyFormSubmission. These are appended
+    to a StudyFormSubmission as a side effect for certain actions.
     """
 
     status = models.CharField(
@@ -74,6 +88,10 @@ class StatusChange(models.Model):
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+
+        ordering = ["created_at"]
 
     def __str__(self) -> str:
         return f"{Statuses(self.new_status).label}: {self.changed_time.strftime("%d-%m-%Y, %H:%M")}"
