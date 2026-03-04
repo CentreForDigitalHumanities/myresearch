bb8c7449f8a35c5503fb23e704fbee041563e271
Michael Villeneuve <m.l.villeneuve@uu.nl>
2026-03-04 14:44:06 +0100

    feat: pg_wipe.sh script

---
+++ scripts/pg_wipe.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+echo This will destroy your current database. You may want to run pg_dump.sh to make a backup first.
+echo Continue? [y/N]
+read y
+
+if [[ $y != y && $y != Y ]]; then
+    echo Aborting
+    exit 1
+fi
+
+if docker compose --profile dev run mr-postgres sh -c \
+	  "rm -r /var/lib/postgresql/data/*"; then
+    echo Success!
+else
+    echo Something went wrong.
+    echo This script must be run as root.
+fi
+    
+
