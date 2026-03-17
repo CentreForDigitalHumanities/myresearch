625247b0cec2dc43ef782a6c7d4fa7b959b59ca3
Edo Storm <111129464+EdoStorm96@users.noreply.github.com>
2026-03-17 10:34:34 +0100

    Update backend/form/types/StepType.py
    Co-authored-by: Xander Vertegaal <73882506+XanderVertegaal@users.noreply.github.com>

--- backend/form/types/StepType.py
+++ backend/form/types/StepType.py
@@ -26,7 +26,7 @@ class StepType(ObjectType):
         lambda: NonNull(StepType),
         required=True,
     )
-    info_text = Field(StepInfoTextType, required=True)
+    info_text = Field(StepInfoTextType)
 
     @staticmethod
     def resolve_info_text(parent, info: ResolveInfo) -> StepInfoText:
