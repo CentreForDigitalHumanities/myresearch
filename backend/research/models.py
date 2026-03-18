92bc7dc0c6785b9de0ba87589b4d96619f23370c
Edo Storm <emtstorm@hotmail.com>
2026-03-18 15:26:48 +0100

    feat: auto generate reference number for Study

--- backend/research/models.py
+++ backend/research/models.py
@@ -1,9 +1,11 @@
+from research.other_models.utils import YearCounter
 from research.other_models.reviews import SubmissionStatus
 from form.models import UserFormSubmission
 from main.models import User, MRPermission
 from main.utils.permission_utils import BaseMRManager
 
-from django.db import models
+from django.db import models, transaction
+from django.utils import timezone
 
 ################
 # Study Object #
@@ -22,6 +24,8 @@ class StudyManager(BaseMRManager):
 
 class Study(models.Model):
 
+    reference = models.CharField(max_length=10, unique=True, blank=True)
+
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
     title = models.CharField(
@@ -37,3 +41,20 @@ class Study(models.Model):
         return self.status_changes.last().status
 
     objects = StudyManager()
+
+    def save(self, *args, **kwargs):
+
+        if not self.reference:
+            with transaction.atomic():
+                year = timezone.now().year % 100  # 2026 -> 26
+
+                counter_obj, _ = YearCounter.objects.select_for_update().get_or_create(
+                    year=year
+                )
+
+                counter_obj.counter += 1
+                counter_obj.save()
+
+                self.reference = f"MR-{year:02d}-{counter_obj.counter:04d}"
+
+        super().save(*args, **kwargs)
