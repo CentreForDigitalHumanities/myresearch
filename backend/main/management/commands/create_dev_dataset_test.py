4ceab02527d655e65226e514cf81545f128e1bc4
Edo Storm <emtstorm@hotmail.com>
2026-03-25 16:43:30 +0100

    fix: remove StepInfoQuestion

--- backend/main/management/commands/create_dev_dataset_test.py
+++ backend/main/management/commands/create_dev_dataset_test.py
@@ -9,7 +9,6 @@ from form.models import (
     QuestionCondition,
     QuestionResponse,
     StepCondition,
-    StepInfoQuestion,
     StepInfoText,
 )
 
@@ -17,7 +16,6 @@ from form.models import (
 # are models that are not essential for the dev dataset or are difficult to
 # generate automatically.
 EXCLUDED_MODELS_FOR_COMPLETENESS_CHECK: list[Type[Model]] = [
-    StepInfoQuestion,
     StepInfoText,
     QuestionResponse,
     StepCondition,
