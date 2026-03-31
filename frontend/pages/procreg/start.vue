392d8186b1bb4b503d5039b8f027f240ca6e8aa8
Edo Storm <emtstorm@hotmail.com>
2026-03-31 11:02:45 +0200

    feat: make big button

--- frontend/pages/procreg/start.vue
+++ frontend/pages/procreg/start.vue
@@ -11,7 +11,8 @@
                 {{ $t(`You are about to register a new study.
                 If you click on the button below, a new study will be created and you will be able to fill in a form.`) }}
             </p>
-            {{ $t("") }}
+            <div class="w-100"></div>
+            <a href="" class="btn btn-primary btn-lg">{{ $t("Register new study") }} >></a>
         </div>
         
 
