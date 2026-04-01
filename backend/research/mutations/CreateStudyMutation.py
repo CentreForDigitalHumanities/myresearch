c96247cf7f9f9c3952e1f6eb2983633ec498c13d
Edo Storm <emtstorm@hotmail.com>
2026-04-01 09:56:29 +0200

    feat: make CreateStudyMutation also make submission

--- backend/research/mutations/CreateStudyMutation.py
+++ backend/research/mutations/CreateStudyMutation.py
@@ -1,8 +1,9 @@
+from form.models import MRForm, UserFormSubmission
 from main.models import User
 from research.models import Study
 from research.types.StudyType import StudyType
 
-from graphene import Field, List, Mutation, ResolveInfo, String
+from graphene import ID, Field, List, Mutation, ResolveInfo, String
 from graphene_django.types import ErrorType
 
 
@@ -11,23 +12,27 @@ class CreateStudyMutation(Mutation):
     study = Field(StudyType)
     errors = List(ErrorType)
 
-    class Arguments:
-        title = String(required=True)
-
     @classmethod
     def mutate(
         cls,
         root: None,
         info: ResolveInfo,
-        title: str,
     ):
 
         user: User = info.context.user
 
         # Check if the user has Create permission
         if Study.can_be_created_by(user):
-            study = Study(title=title, created_by=user)
+            study = Study(created_by=user)
             study.save()
+            # We make a submission using the latest form
+            latest_form = MRForm.objects.last()
+            submission = UserFormSubmission.objects.create(
+                user = user,
+                form = latest_form,
+                study = study,
+            )
+            submission.save()
             return cls(study=study)
         else:
             error = ErrorType(
