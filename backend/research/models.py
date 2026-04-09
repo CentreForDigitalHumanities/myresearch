5be9401c93d5f38aa5b48b557604b25e19134a16
Edo Storm <emtstorm@hotmail.com>
2026-04-09 14:19:42 +0200

    fix: get correct name

--- backend/research/models.py
+++ backend/research/models.py
@@ -46,7 +46,7 @@ class Study(models.Model):
                 study=self,
             )
             response = QuestionResponse.objects.filter(
-                submission=submission,
+                submissions=submission,
                 question_id=name_question.id,
             ).latest("answered_at")
             return (
