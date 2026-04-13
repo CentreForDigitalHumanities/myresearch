79277ece90229ec643e024a0745288a4508ce2a6
Edo Storm <emtstorm@hotmail.com>
2026-04-13 15:59:57 +0200

    fix: unused imports

--- backend/research/models.py
+++ backend/research/models.py
@@ -1,7 +1,6 @@
 from research.other_models.utils import YearCounter
-from research.other_models.reviews import SubmissionStatus
 from form.models import UserFormSubmission
-from main.models import User, MRPermission
+from main.models import User
 from form.models import MRForm, QuestionResponse, UserFormSubmission
 from main.models import User
 from main.utils.permission_utils import BaseMRManager
