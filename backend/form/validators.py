450f1e620b56d2c0331ea9c6843e3c432749376a
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2026-04-15 15:26:37 +0200

    Retrieve fresh OVERVIEW_STEP_SLUG in validator function

--- backend/form/validators.py
+++ backend/form/validators.py
@@ -1,13 +1,13 @@
 from django.core.exceptions import ValidationError
 from django.conf import settings
 
-overview_slug = settings.OVERVIEW_STEP_SLUG.lower()
-
 
 def validate_slug_not_overview(value: str) -> None:
     """
     Make sure the slug is not the same as the overview slug, which is reserved.
     """
+    overview_slug = settings.OVERVIEW_STEP_SLUG.lower()
+
     if value.lower() == overview_slug:
         raise ValidationError(
             f"The slug '{overview_slug}' is reserved and cannot be used."
