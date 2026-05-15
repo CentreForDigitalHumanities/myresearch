cf2f7fa7a4ed69675808ea11ea859482b26fa0a2
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2026-05-15 15:00:30 +0200

    Restore reactivity in StatusBadge status map

--- frontend/components/shared/StatusBadge.vue
+++ frontend/components/shared/StatusBadge.vue
@@ -10,10 +10,9 @@ const props = defineProps<Props>();
 
 const { t } = useI18n();
 
-const STATUS_MAP: Record<
-    SubmissionStatus,
-    { label: string; colorClass: string }
-> = {
+const statusMap = computed<
+    Record<SubmissionStatus, { label: string; colorClass: string }>
+>(() => ({
     [SubmissionStatus.Draft]: { label: t("Draft"), colorClass: "text-bg-gray" },
     [SubmissionStatus.Submitted]: {
         label: t("Submitted"),
@@ -27,11 +26,11 @@ const STATUS_MAP: Record<
         label: t("Rejected"),
         colorClass: "text-bg-danger",
     },
-};
+}));
 </script>
 
 <template>
-    <span :class="`badge rounded-pill ${STATUS_MAP[props.status].colorClass}`">
-        {{ STATUS_MAP[props.status].label }}
+    <span :class="`badge rounded-pill ${statusMap[props.status].colorClass}`">
+        {{ statusMap[props.status].label }}
     </span>
 </template>
