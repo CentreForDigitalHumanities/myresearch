8a3dd8307deb08550983c5f7a6af7ee671e8761e
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2026-06-15 14:03:06 +0200

    Add unit tests and missing i18n

--- backend/form/views.py
+++ backend/form/views.py
@@ -31,7 +31,6 @@ class FileUploadView(LoginRequiredMixin, View):
             status=201,
         )
 
-
 class FileDownloadView(LoginRequiredMixin, BaseFileView):
     """
     Serve a file identified by its UUID.
