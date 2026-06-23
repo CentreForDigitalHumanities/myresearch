2df4546de17123d11f7ed42d46f36c21583a4749
Frank Roordink <frank.roordink@live.nl>
2026-06-23 16:16:09 +0200

    feedback: edo

--- backend/notes/models.py
+++ backend/notes/models.py
@@ -10,7 +10,7 @@ class Note(models.Model):
     title = models.CharField("Note title", max_length=30)
     content = models.TextField("Note content",
                                help_text="For links to other pages within the "
-                               "MR/grant-tool, format url like '/notes/1/'. For links, "
+                               "MR, format url like '/notes/1/'. For links, "
                                "to other websites, format url like 'https://www.uu.nl'. "
                                "In case of a really long word, that is overflowing the formatting, "
                                "go into the source and add the bootstrap class 'text-break' to "
