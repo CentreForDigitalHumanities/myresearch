0fee8dd35a4781f4ad691f7599adaad98deecf02
Edo Storm <emtstorm@hotmail.com>
2026-07-17 16:55:28 +0200

    fix: remove MRDocument fron fixture script

--- backend/form/management/commands/create_form_fixture.py
+++ backend/form/management/commands/create_form_fixture.py
@@ -2,7 +2,7 @@ import json
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
 
-EXCLUDED_MODELS = ["form.QuestionResponse", "form.UserFormSubmission"]
+EXCLUDED_MODELS = ["form.QuestionResponse", "form.UserFormSubmission", "form.MRDocument"]
 
 
 class Command(BaseCommand):
