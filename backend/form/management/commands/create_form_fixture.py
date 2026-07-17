df22a6d1cbbd21f47463f5a298606ece9be3756e
Edo Storm <emtstorm@hotmail.com>
2026-07-17 16:56:46 +0200

    style: black

--- backend/form/management/commands/create_form_fixture.py
+++ backend/form/management/commands/create_form_fixture.py
@@ -2,7 +2,11 @@ import json
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
 
-EXCLUDED_MODELS = ["form.QuestionResponse", "form.UserFormSubmission", "form.MRDocument"]
+EXCLUDED_MODELS = [
+    "form.QuestionResponse",
+    "form.UserFormSubmission",
+    "form.MRDocument",
+]
 
 
 class Command(BaseCommand):
