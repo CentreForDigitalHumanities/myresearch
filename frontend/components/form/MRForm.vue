b3f4e42742432729dad780efb09cf6ad12b4a368
Edo Storm <emtstorm@hotmail.com>
2026-07-28 15:32:19 +0200

    fix: remove unused import

--- frontend/components/form/MRForm.vue
+++ frontend/components/form/MRForm.vue
@@ -5,7 +5,6 @@ import DateQuestion from "../form/DateQuestion.vue";
 import NumberQuestion from "./NumberQuestion.vue";
 import TrueFalseQuestion from "./TrueFalseQuestion.vue";
 import FileUploadQuestion from "../form/FileUploadQuestion.vue";
-import FormSideBar from "../form/FormSideBar.vue";
 import type { Component } from "vue";
 
 // Imported components are treated as 'any', so the linter complains. There is
