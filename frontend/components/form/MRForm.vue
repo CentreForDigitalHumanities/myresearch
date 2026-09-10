cccb33227d233bf1c185079ae123515c9580d619
Edo Storm <emtstorm@hotmail.com>
2026-09-10 13:17:33 +0200

    fix: use toString(), no String()

--- frontend/components/form/MRForm.vue
+++ frontend/components/form/MRForm.vue
@@ -171,7 +171,7 @@ async function handleAddRepeat(question: QuestionWithValue): Promise<void> {
             userFormId,
             repeatableId: question.questionId,
             parentId: props.step.repeatIndex
-                ? String(props.step.repeatIndex)
+                ? props.step.repeatIndex.toString()
                 : null,
         });
     } catch {
@@ -192,7 +192,7 @@ async function handleDeleteRepeat(question: QuestionWithValue): Promise<void> {
         await mutateFormSubmission(props.step, userFormId);
         await deleteQuestionRepeat({
             userFormId,
-            repeatId: String(question.repeatIndex),
+            repeatId: question.repeatIndex?.toString(),
         });
     } catch {
         useNotification(
