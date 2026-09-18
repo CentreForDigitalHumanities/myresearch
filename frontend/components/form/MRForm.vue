64c66ce8b27f819772cd7621ce9b2a801a9615b9
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2026-09-18 13:18:16 +0200

    Replace link with button

--- frontend/components/form/MRForm.vue
+++ frontend/components/form/MRForm.vue
@@ -137,7 +137,7 @@ function isOnlyOccurrence(question: QuestionWithValue): boolean {
         (q) => q.questionId === question.questionId,
     ).length;
     return count === 1;
-};
+}
 
 function isFirstOccurrence(question: QuestionWithValue): boolean {
     const firstIndex = Math.min(
@@ -148,7 +148,7 @@ function isFirstOccurrence(question: QuestionWithValue): boolean {
     const currentIndex =
         question.repeatIndex !== null ? Number(question.repeatIndex) : -1;
     return currentIndex === firstIndex;
-};
+}
 
 async function handleAddRepeat(question: QuestionWithValue): Promise<void> {
     const userFormId = submissionId.value;
@@ -250,17 +250,16 @@ useWatchQuestions(watchedQuestions, () => {
                     "
                     @blur="handleStepNameOverride(question)"
                 />
-                <a
+                <button
                     v-if="question.isRepeatable && !isOnlyOccurrence(question)"
-                    href="#"
-                    class="text-danger"
+                    class="text-danger link-underline-danger border-0"
                     @click="handleDeleteRepeat(question)"
                 >
                     <div class="d-flex align-items-center">
                         <X class="icon" />
                         <div class="ml-3">{{ t("Delete") }}</div>
                     </div>
-                </a>
+                </button>
                 <div v-if="question.isRepeatable && isLastOccurrence(question)">
                     <BSButton
                         variant="primary"
