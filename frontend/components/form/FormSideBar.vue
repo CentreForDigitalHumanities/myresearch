3ba4bdfe37cbcfea4d390cb2d9e4a0cbc83b93d6
Frank Roordink <frank.roordink@live.nl>
2026-09-23 15:04:31 +0200

    Arrows now point in the right direction.

--- frontend/components/form/FormSideBar.vue
+++ frontend/components/form/FormSideBar.vue
@@ -35,7 +35,7 @@ const uniquePageAccordionKey = computed(() => "additionalInfo");
                 <div class="accordion-item">
                     <h2 class="accordion-header">
                         <button
-                            class="accordion-button"
+                            class="accordion-button collapsed"
                             type="button"
                             data-bs-toggle="collapse"
                             :data-bs-target="
